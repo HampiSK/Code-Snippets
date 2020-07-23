dc2d272ba2e8f916a8578142acf70782dada6bce
HampiSK <58185925+HampiSK@users.noreply.github.com>
2020-07-23 22:35:46 +0100

    Update timer.cpp

--- timer.cpp
+++ timer.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <chrono>
 
+/* Simple millisecond timer using chrono library
+ *
+ * Compile with:
+ *
+ *  g++ -std=c++11 timer.cpp
+ */
+
 void timer(int milliseconds)
 {
    auto start = std::chrono::steady_clock::now();
@@ -8,14 +15,14 @@ void timer(int milliseconds)
    {
        auto end = std::chrono::steady_clock::now();
 
-       if (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() > milliseconds)
-       break;
+       if (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() >= milliseconds)
+           break;  
    }
 }
 
 int main()
 {
-    std::cout << "I'll be back in 5 seconds." << std::endl;
-    timer(5000); // 5 sec = 5000 millisec
+    std::cout << "I'll be back in 3000 milliseconds." << std::endl;
+    timer(3000);
     std::cout << "I'm back!" << std::endl;
 }
