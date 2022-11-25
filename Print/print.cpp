130d7d5e81a5efa9e4e4c0f9267ac8a330dd2738
HampiSK <58185925+HampiSK@users.noreply.github.com>
2022-11-25 19:52:00 +0100

    Update print.cpp

--- Print/print.cpp
+++ Print/print.cpp
@@ -4,58 +4,95 @@
 
 /* Simple overloaded print function to print arguments */
 
+constexpr char PRINT_DELIMETER = ' ';
 
-
-/* Non template function to call with zero arguments */
-inline void print() {
-    std::cout<<std::endl;
+/**
+ * @brief Empty print function; flushes stream
+ */
+inline void print()
+{
+    std::cout << std::endl;
 }
 
-/* Template function with 1 argument */
+/**
+ * @brief Print out passed argument
+ */
 template <typename T>
-inline void print(const T & arg) {
+inline void print(const T& arg)
+{
     std::cout << arg << std::endl;
 }
 
-/* Base template used to print all values */
+/**
+ * @brief Print all elements in vector container
+ */
 template <typename T>
-void printAll(const T & arg) {
-    std::cout << arg << " "; // We are not flushing as another values will be printed
-}
-
-/* Using recursion, all arguments will be printed */
-template <typename First, typename... T>
-void printAll(const First & first, const T... args) {
-    printAll(first);    
-    printAll(args...); // Called multiple times based on args count
-}
-
-/* Variadic template with one or more arguments */
-template <typename First, typename... T>
-void print(const First & first, const T... args) {
-    printAll(first);    
-    printAll(args...); // Called multiple times based on args count
-    
-    std::cout<<std::endl; // Flush at the end
-}
-
-/* Print all elements in container */
-template <typename T>
-void print(const std::vector<T> & container) 
+void print(const std::vector<T>& container)
 {
-    for(auto i = container.begin(); i != container.end(); ++i)
-        std::cout<<*i<<' ';
-    std::cout<<std::endl; // Flush at the end
+    for (const auto& element : container)
+    {
+        std::cout << element << PRINT_DELIMETER;
+    }
+}
+
+/**
+ * @brief Print all elements stored in an array. The array is passed as a reference
+ *        to the function, keeping the size information
+ */
+template <typename T, size_t S>
+void print(const T(&container)[S])
+{
+    if (container == nullptr)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < S; ++i)
+    {
+        std::cout << container[i] << PRINT_DELIMETER;
+    }
+}
+
+/**
+ * @brief Print all char array as a string; keeping the size information
+ */
+template <size_t S>
+void print(const char(&container)[S])
+{
+    if (container == nullptr)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < S; ++i)
+    {
+        std::cout << container[i];
+    }
+}
+
+template <typename T>
+inline void printFirst(const T& arg)
+{
+    std::cout << arg << PRINT_DELIMETER;
+}
+
+/**
+ * @brief Using variadic function to print more arguments recursively
+ */
+template <typename First, typename... T>
+void print(const First& first, const T... args)
+{
+    printFirst(first);
+    print(args...); // Called recursively
 }
 
 int main()
 {
-    print(__PRETTY_FUNCTION__); 
-    print(465465,4L,-5,"dsadas",0.1f,'A');
+    print(465465, 4L, -5, "dsadas", 0.1f, 'A');
     print();
     print(50);
-    std::vector<int> vec = {656,46,54,65,4,65,465,4,65,6};
+    std::vector<int> vec = { 656,46,54,65,4,65,465,4,65,6 };
     print(vec);
-    
+
     return 0;
 }
